b8cb93a58fd13d082e2243397dadb8d6210ee5d3
root <root@mage-dcdf69bb8-q2pwd>
2023-11-09 07:07:21 +0800

    Commit WIP to work locally

--- dbt/main/models/med_stock.sql
+++ dbt/main/models/med_stock.sql
@@ -1,19 +1,42 @@
 with latest_snapshot as (
     select
         item,
-        qty,
+        cast(qty as int) as qty,
         opened,
-        size,
+        cast(size as int) as size,
         expiration,
-        dose,
+        cast(dose as int) as dose,
         days,
         last_date,
         row_number() over(partition by item, opened, size order by _mage_updated_at desc) as rownum
     from {{ source('meds_vitamins', 'stock_detail') }}
-
+), unopened as (
+    select
+        item,
+        qty,
+        size,
+        expiration,
+        dose,
+        qty * size as remaining
+    from latest_snapshot
+    where rownum = 1
+        and opened = 'N'
+        and qty > 0
+), opened as (
+    select
+        item,
+        qty,
+        size,
+        expiration,
+        dose
+    from latest_snapshot
+    where rownum = 1
+        and opened = 'Y'
+), next_saturday as (
+    select 
+        6 - extract(dow from now()) sa days_from_sat
 )
 
 select
     *
-from latest_snapshot
-where rownum = 1
+from unopened
